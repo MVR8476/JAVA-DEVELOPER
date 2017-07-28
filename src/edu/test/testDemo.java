9f87c13deab73355d807ac48c710c8a49706c26f
John Doe <mayuresh.ratnaparakhi@neosofttech.com>
2017-07-28 17:44:44 +0530

    new changes added

--- src/edu/test/testDemo.java
+++ src/edu/test/testDemo.java
@@ -5,6 +5,7 @@ public class testDemo {
 	public static void main(String[] args) {
 		System.out.println("Welcome to GIT");
 		System.out.println("Welcome to Master Branch");
+		System.out.println("new 1 line  added in to Master branch");
 
 	}
 
